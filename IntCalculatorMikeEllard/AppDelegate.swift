2fd59b93f62d500431eca11389ca7fd7df8a6e49
Michael Patrick Ellard <ellard@rescuemissionsoftware.com>
2015-01-26 11:20:44 -0800

    Initial Commit

---
+++ IntCalculatorMikeEllard/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  IntCalculatorMikeEllard
+//
+//  Created by Rescue Mission Software on 1/26/15.
+//
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ IntCalculatorMikeEllard/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  IntCalculatorMikeEllard
+//
+//  Created by Rescue Mission Software on 1/26/15.
+//
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
