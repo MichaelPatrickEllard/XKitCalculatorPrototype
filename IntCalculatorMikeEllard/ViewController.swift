41aecc36e2d1480162404651bcd380902ba35b1a
Michael Patrick Ellard <ellard@rescuemissionsoftware.com>
2015-01-26 15:59:53 -0800

    Finished with Exercise 1!

--- IntCalculatorMikeEllard/ViewController.swift
+++ IntCalculatorMikeEllard/ViewController.swift
@@ -10,18 +10,12 @@ import Cocoa
 
 class ViewController: NSViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var displayPanel: NSTextField!
+ 
 
-        // Do any additional setup after loading the view.
+    @IBAction func numberPressed(sender: NSButton) {
+        
+        displayPanel.stringValue = displayPanel.stringValue + sender.title
     }
-
-    override var representedObject: AnyObject? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-
 }
 
